f9ad57554e87c67f1bd8ced53d6339c582d28edf
Samuel Bouaroua <bouaroua.samuel@gmail.com>
2022-06-18 22:46:49 +0200

    chore: vue cli to vite

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,10 +10,6 @@ module.exports = {
     "@vue/prettier",
   ],
   parserOptions: {
-    ecmaVersion: 2020,
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    ecmaVersion: 2021,
   },
 };
