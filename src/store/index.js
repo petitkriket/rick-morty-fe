1b5b3acdedd523d7624644ae37c17faf5436d80b
Samuel Bouaroua <bouaroua.samuel@gmail.com>
2022-06-22 13:24:35 +0200

    refacor: polish vue query use

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,8 @@
 import { createStore } from "vuex";
 
-import characters from "./modules/characters";
-
 export const store = createStore({
   state() {
     return {};
   },
-  modules: {
-    characters,
-  },
+  modules: {},
 });
